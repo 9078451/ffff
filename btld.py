d152b209028f7ec51dc1edec5ef7a32eefb2ee1f
Gamemaster <111993295+9078451@users.noreply.github.com>
2023-08-07 20:17:58 +0200

    Add files via upload

---
+++ btld.py
@@ -0,0 +1,30 @@
+import time
+p = print
+def boot():
+    p("strating sythem boot...")
+    time.sleep(0.3)
+    p("Analyzing Processor speed...")
+    time.sleep(0.3)
+    p("Checking memory modules...")
+    time.sleep(0.3)
+    p("Loading sythem resources... ")
+    time.sleep(0.3)
+    p("Starting network connection...")
+    time.sleep(0.3)
+    p("Booting operating sythem...")
+    time.sleep(0.3)
+    p("Configuring sytheme setting...")
+    time.sleep(0.3)
+    p("Loading user profiles...")
+    time.sleep(0.3)
+    p("Mounting files sythems...")
+    time.sleep(0.3)
+    p("Starting sythem services...")
+    time.sleep(0.3)
+    p("Lunching user interface...")
+    time.sleep(0.3)
+    p("Sythem boot complete !")
+
+
+boot()
+    
